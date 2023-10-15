58eee9ca83a90b9a553c7ce8f9325544b15c4d5e
PiotrPleska <piotrekeska22@gmail.com>
2023-10-15 20:40:31 +0200

    movies by cast

--- z3/src/components/MoviesByCast.vue
+++ z3/src/components/MoviesByCast.vue
@@ -1,15 +1,61 @@
 <template>
   <div class="MoviesByCast">
     <h1>Filmy wg obsady</h1>
+    <ul>
+      <li v-for="cast in uniqueCast" :key="cast">
+        {{ cast }}
+        <ol>
+          <li v-for="movie in moviesByCast(cast)" :key="movie.title">
+            {{ movie.title }}
+          </li>
+        </ol>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import _ from "underscore"; // Import biblioteki Underscore.js
+
 export default {
-  name: "MoviesByCast"
-}
+  name: "MoviesByCast",
+  data() {
+    return {
+      movies: [], // Tablica filmów z pliku JSON
+    };
+  },
+  computed: {
+    uniqueCast() {
+      const cast = _.chain(this.movies)
+          .map("cast")
+          .flatten()
+          .uniq()
+          .value();
+      return cast;
+    },
+  },
+  methods: {
+    moviesByCast(cast) {
+      return _.filter(this.movies, (movie) => _.contains(movie.cast, cast));
+    },
+    loadMoviesFromJson() {
+      fetch("/data/movies.json")
+          .then((response) => response.json())
+          .then((data) => {
+            const first100Movies = data.slice(200, 300);
+            this.movies = first100Movies;
+          })
+          .catch((error) => {
+            console.error("Błąd wczytywania danych:", error);
+          });
+    },
+  },
+  mounted() {
+    this.loadMoviesFromJson();
+  },
+};
 </script>
 
 <style scoped>
-
+/* Dodaj style, jeśli są potrzebne */
 </style>
