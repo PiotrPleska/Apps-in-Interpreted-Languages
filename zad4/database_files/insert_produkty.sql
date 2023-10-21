235ec54ab670d3503c109a2738688463aff39850
kamill10 <kam.cieslak@o2.pl>
2023-10-21 21:13:51 +0200

    insert into products tabel

---
+++ zad4/database_files/insert_produkty.sql
@@ -0,0 +1,10 @@
+use mydb;
+INSERT INTO Produkt (nazwa, opis, cena_jednostkowa, waga_jednostkowa, Kategoria_idKategoria)
+VALUES
+  ('Koszulka', 'Bawełniana koszulka męska', 19.99, 0.3, 1),
+  ('Jeansy', 'Jeansy slim-fit męskie', 49.99, 0.5, 1),
+  ('Sukienka', 'Elegancka sukienka wieczorowa dla kobiet', 79.99, 0.7, 2),
+  ('Buty do biegania', 'Sportowe buty do biegania', 69.99, 0.8, 3),
+  ('Sofa', 'Wygodna sofa 3-osobowa', 299.99, 40.0, 4),
+  ('Naszyjnik', 'Naszyjnik srebrny z kamieniem szlachetnym', 49.99, 0.05, 5),
+  ('Szampon', 'Szampon nawilżający', 9.99, 0.25, 6);
