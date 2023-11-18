6416fc2768ad81e3eb843a9c584a9480ee27b66a
PiotrPleska <piotrekeska22@gmail.com>
2023-11-19 00:35:33 +0100

    minor changes

--- zad4/axios/axios.js
+++ zad4/axios/axios.js
@@ -2,71 +2,71 @@ const axios = require('axios');
 
 //PRODUKT
 // 1. GET app_url/products - zwraca wszystkie produkty
-axios.get('http://localhost:3000/products')
-    .then(response => {
-        console.log('Odpowiedź z serwera:', response.data);
-    })
-    .catch(error => {
-        console.error('Błąd:', error);
-    });
-
-// 2. GET app_url/products/id - zwraca dane produktu o konkretnym identyfikatorze
-axios.get('http://localhost:3000/products/1')
-    .then(response => {
-        console.log('Odpowiedź z serwera:', response.data);
-    })
-    .catch(error => {
-        console.error('Błąd:', error);
-    });
-
-
-// 3.POST app_url/products - dodaje produkt do bazy (parametry w ciele żądania)
-const newProduct = {
-    nazwa: 'Nowszy Produkt',
-    opis: 'To jest przykladowy nowy produkt',
-    cena_jednostkowa: 1.99,
-    waga_jednostkowa: 0.5,
-    Kategoria_idKategoria: 15
-};
-axios.post('http://localhost:3000/products', JSON.stringify(newProduct), {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    })
-    .then(response => {
-        console.log('Dodano nowy produkt:', response.data);
-    })
-    .catch(error => {
-        console.error('Błąd:', error.response.data);
-    });
-
-// 4.PUT app_url/products/id - aktualizuje produkt o konkretnym identyfikatorze, pozostałe parametry produktu w ciele żądania. Można też zrobić PUT app_url/products ze wszystkimi parametrami w ciele żądania.
-const updateProduct = {
-    nazwa: 'Zaktualizowany produkt'
-}
-axios.put('http://localhost:3000/products/1', JSON.stringify(updateProduct), {
-    headers: {
-        'Content-Type': 'application/json',
-    },
-}).then(response => {
-    console.log('Zaktualizowano produkt:', response.data);
-})
-    .catch(error => {
-        console.error('Błąd:', error.response.data);
-    });
-
-// KATEGORIE
-// 1.GET app_url/categories - zwraca wszystkie kategorie
-axios.get('http://localhost:3000/categories')
-    .then(response => {
-        console.log('Odpowiedź z serwera:', response.data);
-    })
-    .catch(error => {
-        console.error('Błąd:', error);
-    });
-
-
-// ZAMOWIENIA
+// axios.get('http://localhost:3000/products')
+//     .then(response => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error);
+//     });
+//
+// // 2. GET app_url/products/id - zwraca dane produktu o konkretnym identyfikatorze
+// axios.get('http://localhost:3000/products/1')
+//     .then(response => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error);
+//     });
+//
+//
+// // 3.POST app_url/products - dodaje produkt do bazy (parametry w ciele żądania)
+// const newProduct = {
+//     nazwa: 'Nowszy Produkt',
+//     opis: 'To jest przykladowy nowy produkt',
+//     cena_jednostkowa: 1.99,
+//     waga_jednostkowa: 0.5,
+//     Kategoria_idKategoria: 15
+// };
+// axios.post('http://localhost:3000/products', JSON.stringify(newProduct), {
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//     })
+//     .then(response => {
+//         console.log('Dodano nowy produkt:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error.response.data);
+//     });
+//
+// // 4.PUT app_url/products/id - aktualizuje produkt o konkretnym identyfikatorze, pozostałe parametry produktu w ciele żądania. Można też zrobić PUT app_url/products ze wszystkimi parametrami w ciele żądania.
+// const updateProduct = {
+//     nazwa: 'Zaktualizowany produkt'
+// }
+// axios.put('http://localhost:3000/products/1', JSON.stringify(updateProduct), {
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// }).then(response => {
+//     console.log('Zaktualizowano produkt:', response.data);
+// })
+//     .catch(error => {
+//         console.error('Błąd:', error.response.data);
+//     });
+//
+// // KATEGORIE
+// // 1.GET app_url/categories - zwraca wszystkie kategorie
+// axios.get('http://localhost:3000/categories')
+//     .then(response => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error);
+//     });
+//
+//
+// // ZAMOWIENIA
 // 1.GET app_url/orders - zwraca wszystkie zamówienia
 axios.get('http://localhost:3000/orders')
     .then(response => {
@@ -75,36 +75,36 @@ axios.get('http://localhost:3000/orders')
     .catch(error => {
         console.error('Błąd:', error);
     });
-
-// 2.POST app_url/orders - dodaje zamówienie (parametry w ciele żądania)
-const data = {
-    data_zamowienia: '2023-10-20 12:30:00',
-    nazwa_uzytkownika: 'John Doe',
-    email: 'john@example.com',
-    numer_telefonu: '123-456-789',
-    Stan_Zamowienia_idStan_Zamowienia: 1,
-    items: [
-        { Produkt_idProdukt: 1, ilosc: 3 },
-        { Produkt_idProdukt: 2, ilosc: 2 },
-    ],
-};
-
-axios.post('http://localhost:3000/orders', data)
-    .then(response => {
-        console.log('Odpowiedź z serwera:', response.data);
-    })
-    .catch(error => {
-        console.error('Błąd:', error);
-    });
-
-
+//
+// // 2.POST app_url/orders - dodaje zamówienie (parametry w ciele żądania)
+// const data = {
+//     data_zamowienia: '2023-10-20 12:30:00',
+//     nazwa_uzytkownika: 'John Doe',
+//     email: 'john@example.com',
+//     numer_telefonu: '123-456-789',
+//     Stan_Zamowienia_idStan_Zamowienia: 1,
+//     items: [
+//         { Produkt_idProdukt: 1, ilosc: 3 },
+//         { Produkt_idProdukt: 2, ilosc: 2 },
+//     ],
+// };
+//
+// axios.post('http://localhost:3000/orders', data)
+//     .then(response => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error);
+//     });
+//
+//
 // 3.PATCH app_url/orders/id - zmiana stanu zamówienia o podanym identyfikatorze, dane w formacie JSON PATCH. Dopuszczalne są inne warianty, np. PUT app_url/orders/id z nowym stanem i pozostałymi parametrami zamówienia w ciele żądania.
 const newState = {
-    numer_telefonu: '666-666-666',
-    nazwa_uzytkownika: 'Jane Doe',
+    // numer_telefonu: '666-666-666',
+    // nazwa_uzytkownika: 'Jane Doe',
     Stan_Zamowienia_idStan_Zamowienia: 4, // Nowy stan zamówienia
 };
-
+//
 axios.patch(`http://localhost:3000/orders/1`, newState)
     .then(response => {
         console.log(response.data);
@@ -112,28 +112,37 @@ axios.patch(`http://localhost:3000/orders/1`, newState)
     .catch(error => {
         console.error(error.response.data);
     });
+//
+//
+//
+// // 4.GET app_url/orders/status/id - pobranie zamówień z określonym stanem
+// const statusIdToFetch = 4;
+//
+// axios.get(`http://localhost:3000/orders/status/${statusIdToFetch}`)
+//     .then((response) => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch((error) => {
+//         console.error('Błąd zapytania do serwera:', error.message);
+//     });
+//
+//
+//
+// // STAN ZAMOWIENIA
+// // 1.GET app_url/status - zwraca wszystkie możliwe stany zamówienia
+// axios.get('http://localhost:3000/status')
+//     .then(response => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error);
+//     });
 
-
-
-// 4.GET app_url/orders/status/id - pobranie zamówień z określonym stanem
-const statusIdToFetch = 4;
-
-axios.get(`http://localhost:3000/orders/status/${statusIdToFetch}`)
-    .then((response) => {
-        console.log('Odpowiedź z serwera:', response.data);
-    })
-    .catch((error) => {
-        console.error('Błąd zapytania do serwera:', error.message);
-    });
-
-
-
-// STAN ZAMOWIENIA
-// 1.GET app_url/status - zwraca wszystkie możliwe stany zamówienia
-axios.get('http://localhost:3000/status')
-    .then(response => {
-        console.log('Odpowiedź z serwera:', response.data);
-    })
-    .catch(error => {
-        console.error('Błąd:', error);
-    });
+// 2. GET app_url/products/id_zamowienia - zwraca produkty w zamówieniu o konkretnym identyfikatorze
+// axios.get('http://localhost:3000/products/order/3')
+//     .then(response => {
+//         console.log('Odpowiedź z serwera:', response.data);
+//     })
+//     .catch(error => {
+//         console.error('Błąd:', error);
+//     });
---
+++ zad4/frontend/sklep/src/components/AdminPanel.vue
@@ -0,0 +1,205 @@
+<template>
+  <div>
+    <div v-if="isLoggedIn">
+      <h2>Witaj w panelu administratora!</h2>
+      <!-- Display unfulfilled orders -->
+      <div v-if="unfulfilledOrders.length > 0">
+        <h3>Niezrealizowane zamówienia</h3>
+        <table class="table table-striped table-bordered">
+          <thead>
+          <tr>
+            <th>Data Zamówienia</th>
+            <th>Wartość</th>
+            <th>Produkt</th>
+            <th>Akcja</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="order in unfulfilledOrders" :key="order.idZamowienie">
+            <td>{{ formatDate(order.data_zamowienia) }}</td>
+            <td>{{ calculateOrderValue(order.products) }}</td>
+            <td>
+              <ul>
+                <li v-for="product in order.products" :key="product.id">
+                  {{ product.nazwa_produktu }} - {{ product.ilosc }} sztuki
+                </li>
+              </ul>
+            </td>
+            <td>
+              <button @click="markAsRealized(order.idZamowienie)" class="btn btn-success">Mark as Realized</button>
+              <button @click="markAsCancelled(order.idZamowienie)" class="btn btn-danger">Mark as Cancelled</button>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <div v-else>
+      <!-- Login form -->
+      <h2>Login to access the Admin Panel</h2>
+      <form @submit.prevent="login">
+        <label for="username">Username:</label>
+        <input type="text" v-model="username" required/>
+        <label for="password">Password:</label>
+        <input type="password" v-model="password" required/>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+      isLoggedIn: false,
+      unfulfilledOrders: [],
+    };
+  },
+  methods: {
+
+    formatDate(dateString) {
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'short'
+      };
+      const formattedDate = new Date(dateString).toLocaleDateString('en-US', options);
+      return formattedDate;
+    },
+    calculateOrderValue(products) {
+      return products.reduce((total, product) => {
+        // Convert the values to numbers before multiplication
+        const price = Number(product.cena_jednostkowa);
+        const quantity = Number(product.ilosc);
+
+        // Check if the values are valid numbers
+        if (!isNaN(price) && !isNaN(quantity)) {
+          // Multiply the price by the quantity for each product
+          return total + (price * quantity);
+        } else {
+          console.error("Invalid price or quantity:", product);
+          return total;
+        }
+      }, 0).toFixed(2); // Assuming you want the result rounded to two decimal places
+    },
+    login() {
+      // Check if the entered credentials are correct
+      if (this.username === "admin" && this.password === "admin") {
+        this.isLoggedIn = true;
+      } else {
+        alert("Invalid username or password");
+      }
+    },
+    // Fetch unfulfilled orders
+    fetchUnfulfilledOrders() {
+      axios.get("http://localhost:3000/orders/status/1")
+          .then((response) => {
+            this.unfulfilledOrders = response.data;
+
+            // Fetch products for each order
+            this.unfulfilledOrders.forEach((order) => {
+              this.fetchProductsForOrder(order.idZamowienie);
+            });
+          })
+          .catch((error) => {
+            console.error("Error fetching unfulfilled orders:", error);
+          });
+    },
+
+    fetchProductsForOrder(orderId) {
+      if (!orderId) {
+        console.error('Invalid orderId:', orderId);
+        return;
+      }
+      // Fetch products for a specific order
+      axios.get(`http://localhost:3000/products/order/${orderId}`)
+          .then((response) => {
+            // Log the response data
+            console.log(`Products for Order ${orderId} Response:`, response.data);
+
+            // Find the corresponding order and update its products
+            const orderIndex = this.unfulfilledOrders.findIndex((order) => order.idZamowienie === orderId);
+            if (orderIndex !== -1) {
+              this.unfulfilledOrders[orderIndex] = {
+                ...this.unfulfilledOrders[orderIndex],
+                products: response.data.map(product => ({
+                  nazwa_produktu: product.nazwa_produktu,
+                  ilosc: product.ilosc,
+                  cena_jednostkowa: product.cena_jednostkowa,
+                })),
+              };
+
+            }
+            console.log(`Updated order ${orderId}:`, this.unfulfilledOrders[orderIndex]);
+          })
+          .catch((error) => {
+            console.error(`Error fetching products for order ${orderId}:`, error);
+          });
+    },
+
+
+    // Mark order as realized
+    markAsRealized(orderId) {
+      // Implement the logic to update the order status to REALIZED (Stan_Zamowienia_idStan_Zamowienia: 4)
+      this.updateOrderStatus(orderId, 4);
+    },
+
+    markAsCancelled(orderId) {
+      // Implement the logic to update the order status to CANCELLED (Stan_Zamowienia_idStan_Zamowienia: 3)
+      this.updateOrderStatus(orderId, 3);
+    },
+
+    updateOrderStatus(orderId, newStatus) {
+      // Make a PATCH request to update the order status
+      const newState = {
+        Stan_Zamowienia_idStan_Zamowienia: newStatus,
+      };
+
+      axios.patch(`http://localhost:3000/orders/${orderId}`, newState)
+          .then(response => {
+            console.log('Response:', response); // Log the entire response object
+
+            if (response.data && response.data.message) {
+              console.log(response.data.message);
+
+              // Assuming you want to update the local state after a successful API call
+              const orderIndex = this.unfulfilledOrders.findIndex(order => order.idZamowienie === orderId);
+              if (orderIndex !== -1) {
+                // Update the local state with the new order status
+                this.unfulfilledOrders[orderIndex] = {
+                  ...this.unfulfilledOrders[orderIndex],
+                  Stan_Zamowienia_idStan_Zamowienia: newStatus,
+                };
+
+              }
+            } else {
+              console.error('Invalid response format:', response.data);
+            }
+          })
+          .catch(error => {
+            console.error(error.response.data);
+          }).finally(() => {
+        window.location.reload();
+      });
+    },
+
+  },
+
+  mounted() {
+    // Fetch unfulfilled orders when the component is mounted
+    this.fetchUnfulfilledOrders();
+  },
+};
+</script>
+
+<style>
+/* Add styles if needed */
+</style>
