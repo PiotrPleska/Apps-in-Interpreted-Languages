e7b49e6b66b2b39bafde7df9563b4af5c2a4a622
PiotrPleska <piotrekeska22@gmail.com>
2023-11-19 01:37:12 +0100

    added table with orders by status

--- zad4/frontend/sklep/src/components/AdminPanel.vue
+++ zad4/frontend/sklep/src/components/AdminPanel.vue
@@ -33,6 +33,27 @@
           </tbody>
         </table>
       </div>
+      <div v-for="(statusOrders, status) in ordersByStatus" :key="status">
+        <h3>{{ getStatusLabel(Number(status)) }}</h3>
+        <table class="table table-striped table-bordered">
+          <!-- Table header -->
+          <thead>
+          <tr>
+            <th>Data Zamówienia</th>
+            <th>Wartość</th>
+          </tr>
+          </thead>
+          <!-- Table body -->
+          <tbody>
+          <!-- Loop through orders with the current status -->
+          <tr v-for="order in statusOrders" :key="order.idZamowienie">
+            <!-- Display order details -->
+            <td>{{ formatDate(order.data_zamowienia) }}</td>
+             <td>{{ calculateOrderValue(order.products) }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <div v-else>
       <!-- Login form -->
@@ -57,10 +78,48 @@ export default {
       password: "",
       isLoggedIn: false,
       unfulfilledOrders: [],
+      orders: [],
     };
   },
+  computed: {
+    ordersByStatus() {
+      return this.orders.reduce((result, order) => {
+        const status = order.Stan_Zamowienia_idStan_Zamowienia;
+        if (!result[status]) {
+          result[status] = [];
+        }
+        result[status].push(order);
+        return result;
+      }, {});
+    },
+  },
   methods: {
-
+    getStatusLabel(status) {
+      switch (status) {
+        case 1:
+          return 'Niezatwierdzone zamówienia';
+        case 2:
+          return 'Zatwierdzone zamówienia';
+        case 3:
+          return 'Anulowane zamówienia';
+        case 4:
+          return 'Zrealizowane zamówienia';
+        default:
+          return 'Nieznany status zamówienia';
+      }
+    },
+    fetchAllOrders() {
+      axios.get('http://localhost:3000/orders')
+          .then(response => {
+            this.orders = response.data;
+            this.orders.forEach((order) => {
+              this.fetchProductsForOrderStatus(order.idZamowienie, order.Stan_Zamowienia_idStan_Zamowienia);
+            });
+          })
+          .catch(error => {
+            console.error('Błąd:', error);
+          });
+    },
     formatDate(dateString) {
       const options = {
         year: 'numeric',
@@ -75,6 +134,10 @@ export default {
       return formattedDate;
     },
     calculateOrderValue(products) {
+      if (!Array.isArray(products) || products.length === 0) {
+        return 0; // or any default value you prefer
+      }
+
       return products.reduce((total, product) => {
         // Convert the values to numbers before multiplication
         const price = Number(product.cena_jednostkowa);
@@ -89,7 +152,8 @@ export default {
           return total;
         }
       }, 0).toFixed(2); // Assuming you want the result rounded to two decimal places
-    },
+
+  },
     login() {
       // Check if the entered credentials are correct
       if (this.username === "admin" && this.password === "admin") {
@@ -122,9 +186,6 @@ export default {
       // Fetch products for a specific order
       axios.get(`http://localhost:3000/products/order/${orderId}`)
           .then((response) => {
-            // Log the response data
-            console.log(`Products for Order ${orderId} Response:`, response.data);
-
             // Find the corresponding order and update its products
             const orderIndex = this.unfulfilledOrders.findIndex((order) => order.idZamowienie === orderId);
             if (orderIndex !== -1) {
@@ -138,7 +199,33 @@ export default {
               };
 
             }
-            console.log(`Updated order ${orderId}:`, this.unfulfilledOrders[orderIndex]);
+          })
+          .catch((error) => {
+            console.error(`Error fetching products for order ${orderId}:`, error);
+          });
+    },
+
+    fetchProductsForOrderStatus(orderId, status) {
+      if (!orderId || !status) {
+        console.error('Invalid orderId or status:', orderId, status);
+        return;
+      }
+
+      axios.get(`http://localhost:3000/products/order/${orderId}`)
+          .then((response) => {
+            const statusOrders = this.orders;
+            const orderIndex = statusOrders.findIndex((order) => order.idZamowienie === orderId);
+
+                if (orderIndex !== -1) {
+                  this.orders[orderIndex] = {
+                    ...this.orders[orderIndex],
+                    products: response.data.map(product => ({
+                      nazwa_produktu: product.nazwa_produktu,
+                      ilosc: product.ilosc,
+                      cena_jednostkowa: product.cena_jednostkowa,
+                    })),
+                  };
+          }
           })
           .catch((error) => {
             console.error(`Error fetching products for order ${orderId}:`, error);
@@ -165,11 +252,7 @@ export default {
 
       axios.patch(`http://localhost:3000/orders/${orderId}`, newState)
           .then(response => {
-            console.log('Response:', response); // Log the entire response object
-
             if (response.data && response.data.message) {
-              console.log(response.data.message);
-
               // Assuming you want to update the local state after a successful API call
               const orderIndex = this.unfulfilledOrders.findIndex(order => order.idZamowienie === orderId);
               if (orderIndex !== -1) {
@@ -196,6 +279,7 @@ export default {
   mounted() {
     // Fetch unfulfilled orders when the component is mounted
     this.fetchUnfulfilledOrders();
+    this.fetchAllOrders();
   },
 };
 </script>
